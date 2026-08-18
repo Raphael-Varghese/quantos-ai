7bfe4e5629ffbcf861824bacc6e818fa933f0983
Code_Boss <s20170468@students.duvalschools.org>
2026-08-18 22:24:46 +0000

    modify for cuda too

--- ai/src/config.py
+++ ai/src/config.py
@@ -6,7 +6,7 @@ import torch
 # Hardware
 # ============================================================
 
-DEVICE = torch.device("cpu")
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 CPU_COUNT = min(
     os.cpu_count() or 4,
